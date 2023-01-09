5769f23ada5415a4f46fb1f41a0bb87781f9066f
VIJAYA BASKAR <vj2k02@gmail.com>
2023-01-09 22:46:26 +0530

    sql setup

---
+++ attendancesystem.sql
@@ -0,0 +1,8 @@
+CREATE TABLE attedencesystem.3IT (
+    id int(10) UNSIGNED AUTO_INCREMENT primary key,
+    firstName varchar(100) not null,
+    lastName varchar(100) not null,
+    emailAddress varchar(200) not null,
+    password varchar(50) not null 
+) ENGINE = MyISAM DEFAULT CHARSET=latin1;
+
