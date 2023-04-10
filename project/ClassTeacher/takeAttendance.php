ec6fb28b94de7f48f757f1ceb58ee3b466fb0edf
VIJAYA BASKAR <vj2k02@gmail.com>
2023-04-10 08:23:42 +0530

    takeAttendanceedit

--- project/ClassTeacher/takeAttendance.php
+++ project/ClassTeacher/takeAttendance.php
@@ -15,7 +15,73 @@
 
 
     // sem and term 
+    $query1 =mysqli_query($conn,"SELECT * from tblsemterm where isActive ='1'");
+    $rrws = mysqli_fetch_array($query1);
+    $semTermId = $rrws['Id'];
 
+    $dataTaken = date("Y-m-d");
+
+    $query2 = mysqli_query($conn,"SELECT * from tblattendance where classId = '$_SESSION[classId]' and dateTimeTaken='$dateTaken'");
+    $count = mysqli_num_rows($query2);
+
+    if($count == 0){
+        // if Record does not exst, insert the new record
+        // omsert the students record into the ttendance table on page load
+        $query3 = mysqli_query($conn,"SELECT * from tblstudents where classId = '$_SESSION[classId]'");
+
+        while($ros = $query3->fetch_assoc())
+        {
+            $query4=mysqli_query($conn,"INSERT into tblattendance(rollNO,classId,semtermId,stauts,dateTimeTaken) value('$ros[rollNumber]','$_SESSION[classId]','0','$dateTaken')");
+        }
+    }
+
+    
+if(isset($_POST['save'])){
+    
+    $admissionNo=$_POST['admissionNo'];
+
+    $check=$_POST['check'];
+    $N = count($admissionNo);
+    $status = "";
+
+
+//check if the attendance has not been taken i.e if no record has a status of 1
+$qurty=mysqli_query($conn,"select * from tblattendance  where classId = '$_SESSION[classId]' and classArmId = '$_SESSION[classArmId]' and dateTimeTaken='$dateTaken' and status = '1'");
+$count = mysqli_num_rows($qurty);
+
+if($count > 0){
+
+    $statusMsg = "<div class='alert alert-danger' style='margin-right:700px;'>Attendance has been taken for today!</div>";
+
+}
+
+    else //update the status to 1 for the checkboxes checked
+    {
+
+        for($i = 0; $i < $N; $i++)
+        {
+                $admissionNo[$i]; //admission Number
+
+                if(isset($check[$i])) //the checked checkboxes
+                {
+                    $qquery=mysqli_query($conn,"update tblattendance set status='1' where rollNo = '$check[$i]'");
+
+                    if ($qquery) {
+
+                        $statusMsg = "<div class='alert alert-success'  style='margin-right:700px;'>Attendance Taken Successfully!</div>";
+                    }
+                    else
+                    {
+                        $statusMsg = "<div class='alert alert-danger' style='margin-right:700px;'>An error Occurred!</div>";
+                    }
+                
+                }
+        }
+    }
+
+
+
+}
 
 ?>
 
@@ -100,7 +166,6 @@
                 </form>
             </div>
                 </div>
-
     </section>
 </body>
 </html>
